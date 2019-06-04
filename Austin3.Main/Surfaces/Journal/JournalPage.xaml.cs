c31141df921399fef3123df5816b8906db169e87
Daniel Kornev <daniel@zetuniverse.com>
2019-06-03 20:59:52 -0700

    added InkAnalyzer

--- Austin3.Main/Surfaces/Journal/JournalPage.xaml.cs
+++ Austin3.Main/Surfaces/Journal/JournalPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,9 +21,42 @@ namespace ZU.Apps.Austin3.Surfaces.Journal
     /// </summary>
     public partial class JournalPage : UserControl
     {
+        private InkAnalyzer newAnalyzer;
+
         public JournalPage()
         {
             InitializeComponent();
+
+            this.Loaded += JournalPage_Loaded;
+
+            this.inkCanvas.Loaded += InkCanvas_Loaded;
         }
-    }
-}
+
+        private void InkCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            newAnalyzer = new InkAnalyzer
+            {
+                AnalysisModes = AnalysisModes.AutomaticReconciliationEnabled
+            };
+            newAnalyzer.ResultsUpdated += new ResultsUpdatedEventHandler(newAnalyzer_ResultsUpdated);
+
+            this.inkCanvas.StrokeCollected += InkCanvas_StrokeCollected;
+        }
+
+        private void InkCanvas_StrokeCollected(object sender, InkCanvasStrokeCollectedEventArgs e)
+        {
+            newAnalyzer.AddStroke(e.Stroke);
+            newAnalyzer.BackgroundAnalyze();
+        }
+
+        private void newAnalyzer_ResultsUpdated(object sender, ResultsUpdatedEventArgs e)
+        {
+            //
+        }
+
+        private void JournalPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    } // class
+} // namespace
