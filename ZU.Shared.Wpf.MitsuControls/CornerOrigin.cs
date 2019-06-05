53a324919777e18720af331055ca73cdb46f491c
Daniel Kornev <daniel@zetuniverse.com>
2019-06-04 20:21:28 -0700

    added Flipping Book control

---
+++ ZU.Shared.Wpf.MitsuControls/CornerOrigin.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZU.Shared.Wpf.Controls
+{
+    public enum CornerOrigin { TopLeft, TopRight, BottomLeft, BottomRight };
+}
---
+++ ZU.Shared.Wpf.MitsuControls/PageStatus.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZU.Shared.Wpf.Controls
+{
+    public enum PageStatus {None, Dragging, DraggingWithoutCapture, DropAnimation, TurnAnimation}
+}
